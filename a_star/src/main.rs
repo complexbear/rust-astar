e09b40df1761b0d45959eecfac98ed8d85e173d7
Matthew Brown <matthew.brown@genomicsengland.co.uk>
2020-04-13 13:07:27 +0100

    Change to use graph crate

--- a_star/src/main.rs
+++ a_star/src/main.rs
@@ -1,16 +1,19 @@
 /*
  * Implementation of A* algorithm for path finding
  */
+use petgraph::dot::{Dot, Config};
+
 mod path;
-use path::{PathNode, create_tree};
+use path::{PathNode, create_graph};
 
 fn main() {
-    let a = PathNode::new(1.0, 0, 0);
-    let b = PathNode::new(2.0, 6, 8);
+    let a = PathNode::new("A", 1.0, 0, 0);
+    let b = PathNode::new("B", 2.0, 6, 8);
 
     println!("node: {}", a);
     println!("node: {}", b);
     println!("travel from a to b: {}", a.travel_cost(&b));
 
-    println!("tree: {}", create_tree());
+    let g = create_graph();
+    println!("{}", Dot::with_config(&g, &[Config::EdgeNoLabel]));
 }
--- a_star/src/path.rs
+++ a_star/src/path.rs
@@ -1,15 +1,19 @@
 use std::fmt;
-use trees::{tr, fr, Tree};
+use petgraph::Graph;
 
+#[derive(Debug)]
+#[derive(Default)]
 pub struct PathNode {
+    name: String,
     cost_weight: f32,
     pos_x: i32,
     pos_y: i32
 }
 
 impl PathNode {
-    pub fn new(w: f32, x: i32, y: i32) -> PathNode {
+    pub fn new(n: &str, w: f32, x: i32, y: i32) -> PathNode {
         PathNode {
+            name: n.to_string(),
             cost_weight: w,
             pos_x: x,
             pos_y: y
@@ -27,11 +31,32 @@ impl PathNode {
 
 impl fmt::Display for PathNode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "PathNode: {}, ({}:{})", self.cost_weight, self.pos_x, self.pos_y)
+        write!(f, "{},{} ({}:{})", self.name, self.cost_weight, self.pos_x, self.pos_y)
     }
 }
 
-pub fn create_tree() -> Tree<PathNode> {
-    let n = tr(PathNode::new(1.0, 0, 0));
-    return n;
+pub fn create_graph() -> Graph<PathNode, PathNode> {
+    /*
+    Basic layout of the paths in the map
+    a = start
+    e = end
+    |         __ c(3,5)_______
+    |        /                \
+    |   b(1,3)                 e(10,3)
+    |   /                     /
+    |   |                d(7,1)
+    a(0,0)______________/
+    */
+    let mut deps = Graph::<PathNode, PathNode>::new();
+    let a = deps.add_node(PathNode::new("A",1.0, 0, 0));
+    let b = deps.add_node(PathNode::new("B",1.0, 1, 3));
+    let c = deps.add_node(PathNode::new("C",1.0, 3, 5));
+    let d = deps.add_node(PathNode::new("D",1.0, 7, 1));
+    let e = deps.add_node(PathNode::new("E",1.0, 10, 3));
+
+    deps.extend_with_edges(&[
+        (a, b), (b, c), (c, e),
+        (a, d), (d, e)
+    ]);
+    return deps;
 }
